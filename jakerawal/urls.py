28cba6fbac294d7e7c75eb2cadf4d4898b68edd4
Jake Rawal (k22001853) <jake.rawal3@gmail.com>
2024-01-17 12:46:12 +0000

    Start Nav Bar Styling

--- jakerawal/urls.py
+++ jakerawal/urls.py
@@ -17,11 +17,17 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from personalweb import views
+from django.conf.urls.static import static
+from django.conf import settings
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 #Plan out the Website - Design and CSS, Javascript and so forth
 #Static Folder Holds CSS
