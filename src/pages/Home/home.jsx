841b6e7259d36aa3c9980ac02cdb6ef2e48430c6
Jake Rawal (k22001853) <jake.rawal3@gmail.com>
2024-01-18 21:28:31 +0000

    Complete Home Page

--- src/pages/Home/home.jsx
+++ src/pages/Home/home.jsx
@@ -3,11 +3,14 @@ import './Home.css'
 function Home() {
     return (
         <>
-        <div>
-            <div id="prize-lines" class="prize-filter"></div>
-                <div id="prize-shadow" class="prize-filter"></div>
-                    <h1 id="prize-text"> Jake Rawal - Software Engineer </h1>
-                </div>
+        <div id='prize'>
+        <div id="prize-lines" class="prize-filter"></div>
+        <div id="prize-shadow" class="prize-filter"></div>
+        <h2 id="prize-text">
+            Jake Rawal - Software Engineer     
+        </h2>
+        <h1 id="prize-text"></h1>
+        </div>
         </>
 
     )
