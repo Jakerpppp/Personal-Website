2f531b9b2fadc71dd668ccce55637f0649a63219
Jake Rawal (k22001853) <jake.rawal3@gmail.com>
2024-01-20 00:18:04 +0000

    Cards Complete

--- src/components/Card/card.jsx
+++ src/components/Card/card.jsx
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
 import './card.css';
+import ClickableLogo from '../ClickableLogo/ClickableLogo';
 
-function Card({ image, title, description }) {
+function Card({image, title, short_description, techUsed, img1, img2, img3, img4, img5, logo1, logo2, long_description }) {
   const [showTextScreen, setShowTextScreen] = useState(false);
 
   const handleButtonClick = () => {
@@ -18,16 +19,40 @@ function Card({ image, title, description }) {
         <img src={image} alt="Card Image" className="card-image" />
         <div className="card-content">
           <h2 className="card-title">{title}</h2>
-          <p className="card-description">{description}</p>
+          <p className="card-description">{short_description}</p>
           <button className="card-button">More Info!</button>
         </div>
       </div>
 
       {showTextScreen && (
         <div className="text-screen" onClick={handleCloseTextScreen}>
-          {/* Your text screen content goes here */}
-          <h2>{title}</h2>
-          <p>{description}</p>
+          <div className="text-screen-content">
+            <h2 className="text-screen-title">{title}</h2>
+            
+            {logo1 && (
+              <ClickableLogo picture={button1Logo.picture} link={button1Logo.link} />
+            )}
+
+            {logo2 && (
+              <ClickableLogo picture={button2Logo.picture} link={button2Logo.link} />
+            )}
+
+            <div className="tech-used">
+              {img1 && <img src={img1} alt="Tech Image 1" />}
+              {img2 && <img src={img2} alt="Tech Image 2" />}
+              {img3 && <img src={img3} alt="Tech Image 3" />}
+              {img4 && <img src={img4} alt="Tech Image 4" />}
+              {img5 && <img src={img5} alt="Tech Image 5" />}
+            </div>
+
+            {techUsed && <p className="tech-used-text">{techUsed}</p>}
+
+            <hr />
+
+            <p className="long-description">
+              {long_description}
+            </p>
+          </div>
         </div>
       )}
     </>
--- src/pages/Projects/projects.jsx
+++ src/pages/Projects/projects.jsx
@@ -6,10 +6,19 @@ function Projects() {
     return (
         <>
         <div style={{ marginTop: '100px' }}>
+        <h1>Projects</h1>
             <Card 
                 image={"github-mark-white.png"} 
-                title={"Test"} 
-                description={"Hello there"}/>
+                title={"Project Showcase Website"} 
+                short_description={"A Website developed in React to showcase my Coding Projects"}
+                techUsed={'I used all the Tech in the entire world'}
+
+
+
+
+
+
+                />
             <Card />
             <Card />
             <Card />
